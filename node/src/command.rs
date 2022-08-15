9a071fa9acdf069a825d91f2beb232de33583521
Sergey Kovalev <sergey@weedydidie.com>
2022-08-15 12:03:19 +0300

    chore: cleanup links

--- node/src/command.rs
+++ node/src/command.rs
@@ -12,7 +12,7 @@ use std::sync::Arc;
 
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
-		"Substrate Node".into()
+		"FinalBiome Node".into()
 	}
 
 	fn impl_version() -> String {
@@ -28,11 +28,11 @@ impl SubstrateCli for Cli {
 	}
 
 	fn support_url() -> String {
-		"support.anonymous.an".into()
+		"finalbiome.net".into()
 	}
 
 	fn copyright_start_year() -> i32 {
-		2017
+		2022
 	}
 
 	fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
